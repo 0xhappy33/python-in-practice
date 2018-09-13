4f233b947ff9d4fc57a87bd8f23c52c79b06534b
truongvanha <hachange.sgroup@gmail.com>
2018-09-13 08:49:47 +0700

    Add readme and more function

--- index.py
+++ index.py
@@ -101,13 +101,67 @@ def for_else():
     else:
         print('did not break out of for loop')
 
+
+def convert_list_comma():
+    """ Convert list to comma separated string"""
+    items = ['foo', 'bar', 'xyz']
+    print(', '.join(items))
+    """ list of numbers to comma separated """
+    numbers = [2, 3, 5, 10]
+    print(','.join(map(str, numbers)))
+    """ list of mix data """
+    data = [2, 'hello', 3, 3.4]
+    print(','.join(map(str, data)))
+
+
+def merge_dict():
+    """ Merge dict's """
+    d1 = {'a': 1}
+    d2 = {'b': 2}
+    print({**d1, **d2})
+    print(dict(d1.items() | d2.items()))
+    d1.update(d2)
+    print(d1)
+
+
+def maxIndex(lst):
+    return min(range(len(lst)), key=lst.__getitem__)
+
+
+def minIndex(lst):
+    return max(range(len(lst)), key=lst.__getitem__)
+
+
+def find_min_max_in_list():
+    lst = [40, 11, 9, 10, 52, 7, 15]
+    print('Max is: ', lst[maxIndex(lst)], ' at ', maxIndex(lst))
+    print('Min is: ', lst[minIndex(lst)], ' at ', minIndex(lst))
+
+
+from collections import OrderedDict
+
+
+def remove_duplicate():
+    """ remove duplicate in list """
+    lst = [2, 3, 3, 4, 5, 1, 2, 10, 9]
+    new_items2 = list(set(lst))
+    print(new_items2)
+    """ Using OrderedDict """
+    items = ['foo', 'bar', 'foo', 'bar', 'foo']
+    print(list(OrderedDict.fromkeys(items).keys()))
+
+
 if __name__ == "__main__":
-    # make_single_string()
-    # find_the_most_frequent_value()
-    # is_anagrams("ab", "cba")
-    # reverse_string()
-    # transpose_array()
-    # compare_chains(6)
-    # chained_function_call()
-    # get_dictionary()
+    make_single_string()
+    find_the_most_frequent_value()
+    is_anagrams("ab", "cba")
+    reverse_string()
+    transpose_array()
+    compare_chains(6)
+    chained_function_call()
+    get_dictionary()
     sort_dictionary()
+    convert_list_comma()
+    merge_dict()
+    find_min_max_in_list()
+    remove_duplicate()
